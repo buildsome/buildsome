18880cab0c5fc13819d0c509315dc6090deb9755
Eyal Lotem <eyal.lotem@gmail.com>
2014-02-20 16:08:21 +0200

    Add Makefile parsing

---
+++ Lib/Makefile.hs
@@ -0,0 +1,54 @@
+module Lib.Makefile
+  ( Target(..)
+  , Makefile(..)
+  , makefile
+  ) where
+
+import Control.Applicative
+import qualified Data.Attoparsec.ByteString.Char8 as P
+import qualified Data.ByteString.Char8 as BS
+
+data Target = Target
+  { outputPaths :: [FilePath]
+  , inputHints :: [FilePath]
+  , cmds :: [String]
+  } deriving (Show)
+
+newtype Makefile = Makefile
+  { targets :: [Target] }
+  deriving (Show)
+
+isSeparator :: Char -> Bool
+isSeparator x = P.isSpace x || x == ':'
+
+word :: P.Parser BS.ByteString
+word = P.takeWhile1 (not . isSeparator)
+
+horizSpace :: P.Parser Char
+horizSpace = P.satisfy $ \x -> x == ' ' || x == '\t'
+
+lineWords :: P.Parser [BS.ByteString]
+lineWords = many (many horizSpace *> word <* many horizSpace)
+
+cmdLine :: P.Parser BS.ByteString
+cmdLine =
+  (P.char '\t' *> P.takeTill (== '\n')) <|>
+  (BS.empty <$ (many horizSpace *> P.char '\n'))
+
+target :: P.Parser Target
+target = do
+  outputPaths <- lineWords
+  _ <- P.char ':'
+  inputPaths <- lineWords
+  _ <- P.char '\n'
+  cmdLines <- filter (not . BS.null) <$> many cmdLine
+  let unpack = map BS.unpack
+  return Target
+    { outputPaths = unpack outputPaths
+    , inputHints = unpack inputPaths
+    , cmds = unpack cmdLines
+    }
+
+makefile :: P.Parser Makefile
+makefile =
+  (Makefile <$> many target) <* P.endOfInput
