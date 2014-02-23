fe82fc2a0dedd2f787fc7c5051168c68d668a281
Eyal Lotem <eyal.lotem@gmail.com>
2014-02-23 21:51:55 +0200

    EnumSophia utility

---
+++ EnumSophia.hs
@@ -0,0 +1,17 @@
+{-# LANGUAGE OverloadedStrings #-}
+import Control.Monad
+import Database.Sophia as Sophia
+import System.Environment (getArgs)
+import qualified Data.ByteString as BS
+
+showBS x = "BS.pack " ++ show (BS.unpack x)
+
+main = do
+  [dbDir] <- getArgs
+  withEnv $ \env -> do
+    openDir env ReadOnly DisallowCreation dbDir
+    withDb env $ \db -> do
+      withCursor db Sophia.GT "" $ \cursor -> do
+        rows <- fetchCursorAll cursor
+        forM rows $ \(key, val) ->
+          putStrLn $ concat [showBS key, " = ", showBS val]
