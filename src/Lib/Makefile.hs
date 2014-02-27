ddbb243223cfe691d3e10b28ea99aa0e04b6c532
Eyal Lotem <eyal.lotem@gmail.com>
2014-02-27 17:08:39 +0200

    Support meta-variables in Makefile

--- src/Lib/Makefile.hs
+++ src/Lib/Makefile.hs
@@ -7,6 +7,8 @@ module Lib.Makefile
 import Control.Applicative
 import Control.Monad
 import Data.List (partition)
+import Data.Maybe (fromMaybe, listToMaybe)
+import System.FilePath (takeDirectory, takeFileName)
 import qualified Data.Char as C
 import qualified Text.Parsec as P
 
@@ -44,10 +46,64 @@ skippedLine = void (P.many1 P.space) <|> comment
 lineWords :: Parser [String]
 lineWords = many (many horizSpace *> word <* many horizSpace)
 
-cmdLine :: Parser String
-cmdLine = do
+parseLiteralString :: Parser String
+parseLiteralString = do
+  x <- P.char '\''
+  str <- concat <$> many p
+  y <- P.char '\''
+  return $ concat [[x], str, [y]]
+  where
+    p = escapeSequence <|>
+        ((: []) <$> P.satisfy (`notElem` "\n'"))
+
+escapeSequence :: Parser String
+escapeSequence = do
+  esc <- P.char '\\'
+  code <- P.anyChar
+  return [esc, code]
+
+variable :: [FilePath] -> [FilePath] -> Parser String
+variable outputPaths inputPaths = do
+  _ <- P.char '$'
+  varId <- P.anyChar
+  case varId of
+    '(' -> do
+      (multiVarId, multiType) <- (,) <$> P.anyChar <*> P.anyChar
+      _ <- P.char ')'
+      selector <-
+        case multiType of
+        'D' -> return takeDirectory
+        'F' -> return takeFileName
+        _ -> fail $ concat
+             [ "Invalid selector "
+             , show multiType, " in sequence: $("
+             , [multiVarId, multiType], ")"
+             ]
+      varSelect multiVarId selector
+    _ -> varSelect varId id
+  where
+    getFirst err paths = fromMaybe err $ listToMaybe paths
+    varSelect varId toString =
+      case varId of
+      '@' -> return $ toString $ getFirst "No first output for @ variable" outputPaths
+      '<' -> return $ toString $ getFirst "No first input for < variable" inputPaths
+      '^' -> return $ unwords $ map toString inputPaths
+      '|' -> error "TODO: order-only inputs"
+      _ -> fail $ "Invalid variable id: " ++ [varId]
+
+parseCmdChar :: [FilePath] -> [FilePath] -> Parser String
+parseCmdChar outputPaths inputPaths =
+  escapeSequence <|>
+  variable outputPaths inputPaths <|>
+  (: []) <$> P.satisfy (/= '\n')
+
+interpolateCmdLine :: [FilePath] -> [FilePath] -> Parser String
+interpolateCmdLine outputPaths inputPaths = concat <$> many (parseLiteralString <|> parseCmdChar outputPaths inputPaths)
+
+cmdLine :: [FilePath] -> [FilePath] -> Parser String
+cmdLine outputPaths inputPaths = do
   res <-
-    (P.char '\t' *> tillEndOfLine <* P.char '\n') <|>
+    (P.char '\t' *> interpolateCmdLine outputPaths inputPaths <* P.char '\n') <|>
     ("" <$ skippedLine)
   return res
 
@@ -58,7 +114,7 @@ target = do
   _ <- P.char ':'
   inputPaths <- lineWords
   _ <- P.char '\n'
-  cmdLines <- filter (not . null) <$> many (P.try cmdLine)
+  cmdLines <- filter (not . null) <$> many (cmdLine outputPaths inputPaths)
   return Target
     { targetOutputPaths = outputPaths
     , targetInputHints = inputPaths
