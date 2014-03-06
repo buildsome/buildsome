97594f46093a5004843dfcde832bde4606b7476f
Eyal Lotem <eyal.lotem@gmail.com>
2014-03-06 22:58:51 +0200

    Add lens/traversals for Makefile.Types to access the FilePath components

--- src/Lib/Makefile/Types.hs
+++ src/Lib/Makefile/Types.hs
@@ -1,13 +1,15 @@
 module Lib.Makefile.Types
   ( TargetType(..)
-  , FilePattern(..), InputPat(..)
-  , Target, Pattern
-  , Makefile(..)
+  , FilePattern(..), onFilePatternPaths
+  , InputPat(..), onInputPatPaths
+  , Target, onTargetPaths
+  , Pattern, onPatternPaths
+  , Makefile(..), onMakefilePaths
   ) where
 
+import Control.Applicative (Applicative(..), (<$>))
+import Data.Traversable (traverse)
 import Lib.StringPattern (StringPattern)
-
-type Target = TargetType FilePath FilePath
 
 data TargetType output input = Target
   { targetOutputs :: [output]
@@ -16,12 +18,14 @@ data TargetType output input = Target
   , targetCmds :: [String]
   } deriving (Show)
 
+type Target = TargetType FilePath FilePath
+
 data FilePattern = FilePattern
   { filePatternDirectory :: FilePath
   , filePatternFile :: StringPattern
   } deriving (Show)
 
-data InputPat = InputPath String | InputPattern FilePattern
+data InputPat = InputPath FilePath | InputPattern FilePattern
   deriving (Show)
 
 type Pattern = TargetType FilePattern InputPat
@@ -31,3 +35,35 @@ data Makefile = Makefile
   , makefilePatterns :: [Pattern]
   , makefilePhonies :: [FilePath]
   } deriving (Show)
+
+-- Filepath lens boilerplate:
+
+onTargetPaths :: Applicative f => (FilePath -> f FilePath) -> Target -> f Target
+onTargetPaths f (Target outputs inputs orderOnlyInputs cmds) =
+  Target
+  <$> traverse f outputs
+  <*> traverse f inputs
+  <*> traverse f orderOnlyInputs
+  <*> pure cmds
+
+onFilePatternPaths :: Functor f => (FilePath -> f FilePath) -> (FilePattern -> f FilePattern)
+onFilePatternPaths f (FilePattern dir file) = (`FilePattern` file) <$> f dir
+
+onInputPatPaths :: Functor f => (FilePath -> f FilePath) -> InputPat -> f InputPat
+onInputPatPaths f (InputPath x) = InputPath <$> f x
+onInputPatPaths f (InputPattern x) = InputPattern <$> onFilePatternPaths f x
+
+onPatternPaths :: Applicative f => (FilePath -> f FilePath) -> Pattern -> f Pattern
+onPatternPaths f (Target outputs inputs orderOnlyInputs cmds) =
+  Target
+  <$> traverse (onFilePatternPaths f) outputs
+  <*> traverse (onInputPatPaths f) inputs
+  <*> traverse (onInputPatPaths f) orderOnlyInputs
+  <*> pure cmds
+
+onMakefilePaths :: Applicative f => (FilePath -> f FilePath) -> Makefile -> f Makefile
+onMakefilePaths f (Makefile targets patterns phonies) =
+  Makefile
+  <$> traverse (onTargetPaths f) targets
+  <*> traverse (onPatternPaths f) patterns
+  <*> traverse f phonies
