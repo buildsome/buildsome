53d869c01eb3b097ee851d175cda1f05e9ffd959
Eyal Lotem <eyal.lotem@gmail.com>
2014-03-07 01:30:50 +0200

    Oops, stem need not include the pattern dir

--- src/Lib/Makefile/InstantiatePattern.hs
+++ src/Lib/Makefile/InstantiatePattern.hs
@@ -14,16 +14,11 @@ plugFilePattern :: StringPattern.Match -> FilePattern -> FilePath
 plugFilePattern match (FilePattern dir file) = dir </> StringPattern.plug match file
 
 instantiatePatternByMatch :: StringPattern.Match -> Pattern -> Target
-instantiatePatternByMatch match target@(Target outputs inputs ooInputs cmds) =
+instantiatePatternByMatch match (Target outputs inputs ooInputs cmds) =
   interpolateCmds mStem $
   Target pluggedOutputs pluggedInputs pluggedOOInputs cmds
   where
-    firstOutput =
-      case outputs of
-      [] -> error ("At least one output is required in pattern rules: " ++ show target)
-      (x:_) -> x
-    patDir = filePatternDirectory firstOutput
-    mStem = Just (patDir </> matchPlaceHolder match)
+    mStem = Just (matchPlaceHolder match)
     plugInputMatch (InputPattern pat) = plugFilePattern match pat
     plugInputMatch (InputPath str) = str
     pluggedOutputs  = map (plugFilePattern match) outputs
