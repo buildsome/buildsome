471ddf859c1e913000cc2cadff62cf7456dde50f
Eyal Lotem <eyal.lotem@gmail.com>
2014-03-15 22:56:03 +0200

    Use cabal to build everything, rather than a Makefile

--- Setup.hs
+++ Setup.hs
@@ -1,2 +1,8 @@
 import Distribution.Simple
-main = defaultMain
+import System.Process
+main = defaultMainWithHooks simpleUserHooks
+  { preBuild = \_ _ -> do
+      putStrLn "Building fs_override.so"
+      system "gcc -o fs_override.so -g -Wall -Wextra -Winit-self -shared -fPIC fs_override.c -ldl -lbsd"
+      return (Nothing, [])
+  }
