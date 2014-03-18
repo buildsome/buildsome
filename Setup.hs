de6e0940b7055599a5dd82206dcc9130c9c56ded
Eyal Lotem <eyal.lotem@gmail.com>
2014-03-18 17:45:18 +0200

    Check execution of gcc

--- Setup.hs
+++ Setup.hs
@@ -1,8 +1,11 @@
 import Distribution.Simple
 import System.Process
+import System.Exit
 main = defaultMainWithHooks simpleUserHooks
   { preBuild = \_ _ -> do
       putStrLn "Building fs_override.so"
-      system "gcc -o fs_override.so -g -Wall -Wextra -Winit-self -shared -fPIC -D_GNU_SOURCE fs_override.c canonize_path.c -ldl -lbsd"
-      return (Nothing, [])
+      exitCode <- system "gcc -o fs_override.so -g -Wall -Wextra -Winit-self -shared -fPIC -D_GNU_SOURCE fs_override.c canonize_path.c -ldl -lbsd"
+      case exitCode of
+        ExitSuccess -> return (Nothing, [])
+        ExitFailure i -> fail $ "fs_override.so build failed with " ++ show i
   }
