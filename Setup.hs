62867f1b0e2701c8ed3fea7ff0cb2d3d9c9db695
Eyal Lotem <eyal.lotem@gmail.com>
2014-03-26 17:19:21 +0200

    -Werror on fs_override.so

--- Setup.hs
+++ Setup.hs
@@ -4,7 +4,7 @@ import System.Exit
 main = defaultMainWithHooks simpleUserHooks
   { preBuild = \_ _ -> do
       putStrLn "Building fs_override.so"
-      exitCode <- system "gcc -o fs_override.so -g -Wall -Wextra -Winit-self -shared -fPIC -D_GNU_SOURCE fs_override.c canonize_path.c -ldl -lbsd"
+      exitCode <- system "gcc -o fs_override.so -g -Wall -Werror -Wextra -Winit-self -shared -fPIC -D_GNU_SOURCE fs_override.c canonize_path.c -ldl -lbsd"
       case exitCode of
         ExitSuccess -> return (Nothing, [])
         ExitFailure i -> fail $ "fs_override.so build failed with " ++ show i
