f1c9b995bfed27a7fd1898b9550d33b0c420d554
Eyal Lotem <eyal.lotem@gmail.com>
2014-03-29 23:18:17 +0300

    Lib.ByteString: Export chopTrailingNewline and guaranteeTrailingNewline. import ByteString

--- src/Lib/ByteString.hs
+++ src/Lib/ByteString.hs
@@ -1,31 +1,46 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Lib.ByteString
   ( truncateAt
   , unprefixed, unsuffixed
   , strictify, lazify
+  , chopTrailingNewline, guaranteeTrailingNewline
   ) where
 
+import Data.ByteString (ByteString)
+import Data.Monoid
 import Data.Word
 import qualified Data.ByteString as BS
+import qualified Data.ByteString.Char8 as BS8
 import qualified Data.ByteString.Lazy as BSL
 
-truncateAt :: Word8 -> BS.ByteString -> BS.ByteString
+chopTrailingNewline :: ByteString -> ByteString
+chopTrailingNewline bs
+  | "\n" `BS8.isSuffixOf` bs = BS8.init bs
+  | otherwise = bs
+
+guaranteeTrailingNewline :: ByteString -> ByteString
+guaranteeTrailingNewline bs
+  | "\n" `BS8.isSuffixOf` bs = bs
+  | otherwise = bs <> "\n"
+
+truncateAt :: Word8 -> ByteString -> ByteString
 truncateAt z bs =
   case BS.split z bs of
   [] -> BS.empty
   (x:_) -> x
 
-unprefixed :: BS.ByteString -> BS.ByteString -> Maybe BS.ByteString
+unprefixed :: ByteString -> ByteString -> Maybe ByteString
 unprefixed prefix full
   | prefix `BS.isPrefixOf` full = Just $ BS.drop (BS.length prefix) full
   | otherwise = Nothing
 
-unsuffixed :: BS.ByteString -> BS.ByteString -> Maybe BS.ByteString
+unsuffixed :: ByteString -> ByteString -> Maybe ByteString
 unsuffixed suffix full
   | suffix `BS.isSuffixOf` full = Just $ BS.take (BS.length full - BS.length suffix) full
   | otherwise = Nothing
 
-strictify :: BSL.ByteString -> BS.ByteString
+strictify :: BSL.ByteString -> ByteString
 strictify = BS.concat . BSL.toChunks
 
-lazify :: BS.ByteString -> BSL.ByteString
+lazify :: ByteString -> BSL.ByteString
 lazify = BSL.fromChunks . (: [])
