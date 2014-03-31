969b347f49cb0501066319efb3f22dd6ab0b9fe2
Eyal Lotem <eyal.lotem@gmail.com>
2014-03-31 18:38:53 +0300

    Move c code into cbits/

--- Setup.hs
+++ Setup.hs
@@ -1,10 +1,13 @@
 import Distribution.Simple
 import System.Process
 import System.Exit
+
+sources = ["cbits/fs_override.c", "cbits/canonize_path.c"]
+
 main = defaultMainWithHooks simpleUserHooks
   { preBuild = \_ _ -> do
       putStrLn "Building fs_override.so"
-      exitCode <- system "gcc -o fs_override.so -g -Wall -Werror -Wextra -Winit-self -shared -fPIC -D_GNU_SOURCE fs_override.c canonize_path.c -ldl"
+      exitCode <- system $ "gcc -o fs_override.so -g -Wall -Werror -Wextra -Winit-self -shared -fPIC -D_GNU_SOURCE " ++ unwords sources ++ " -ldl"
       case exitCode of
         ExitSuccess -> return (Nothing, [])
         ExitFailure i -> fail $ "fs_override.so build failed with " ++ show i
