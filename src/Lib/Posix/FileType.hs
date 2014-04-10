5ee5344cab2fc38cd5485eb605b64a8ada26fd3a
Eyal Lotem <eyal.lotem@gmail.com>
2014-04-10 03:19:19 +0300

    Add Lib.Posix.FileType

---
+++ src/Lib/Posix/FileType.hs
@@ -0,0 +1,30 @@
+{-# LANGUAGE DeriveGeneric #-}
+module Lib.Posix.FileType
+  ( FileType(..), fileTypeOfStat
+  ) where
+
+import Data.Binary (Binary)
+import GHC.Generics (Generic)
+import qualified System.Posix.ByteString as Posix
+
+data FileType
+  = BlockDevice
+  | CharacterDevice
+  | Directory
+  | NamedPipe
+  | RegularFile
+  | Socket
+  | SymbolicLink
+  deriving (Generic, Eq, Show)
+instance Binary FileType
+
+fileTypeOfStat :: Posix.FileStatus -> FileType
+fileTypeOfStat stat
+  | Posix.isBlockDevice stat = BlockDevice
+  | Posix.isCharacterDevice stat = CharacterDevice
+  | Posix.isDirectory stat = Directory
+  | Posix.isNamedPipe stat = NamedPipe
+  | Posix.isRegularFile stat = RegularFile
+  | Posix.isSocket stat = Socket
+  | Posix.isSymbolicLink stat = SymbolicLink
+  | otherwise = error "Unrecognized file type"
