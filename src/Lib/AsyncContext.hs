e77854ba4a0f1d4b44e8cba167387bd0f6244251
Eyal Lotem <eyal.lotem@gmail.com>
2014-04-14 15:05:30 +0300

    Oops: modifyMVarMasked was used which masked way too many things

--- src/Lib/AsyncContext.hs
+++ src/Lib/AsyncContext.hs
@@ -4,34 +4,31 @@ module Lib.AsyncContext
   ) where
 
 import Control.Concurrent.Async
-import Control.Concurrent.MVar
+import Data.IORef
 import Data.IntMap (IntMap)
 import Lib.Fresh (Fresh)
+import Lib.IORef (atomicModifyIORef'_)
 import qualified Control.Exception as E
 import qualified Data.IntMap as IntMap
 import qualified Lib.Fresh as Fresh
 
 data AsyncContext = AsyncContext
   { _ctxFreshNames :: Fresh Int
-  , _ctxCancelActions :: MVar (IntMap (IO ()))
+  , _ctxCancelActions :: IORef (IntMap (IO ()))
   }
 
 new :: (AsyncContext -> IO a) -> IO a
 new body = do
   freshNames <- Fresh.new 0
-  cancelActionsVar <- newMVar IntMap.empty
+  cancelActionsVar <- newIORef IntMap.empty
   body (AsyncContext freshNames cancelActionsVar) `E.finally` do
-    cancelActions <- readMVar cancelActionsVar
+    cancelActions <- readIORef cancelActionsVar
     sequence_ $ IntMap.elems cancelActions
 
 spawn :: AsyncContext -> IO a -> IO (Async a)
 spawn (AsyncContext freshNames cancelActionsVar) act = do
   name <- Fresh.next freshNames
-  modifyMVarMasked cancelActionsVar $ \cancelActions -> do
-    actAsync <-
-      async $
-      act `E.finally` modifyMVar_ cancelActionsVar (return . IntMap.delete name)
-    return
-      ( IntMap.insert name (cancel actAsync) cancelActions
-      , actAsync
-      )
+  E.mask $ \restore -> do
+    actAsync <- async $ restore act `E.finally` atomicModifyIORef'_ cancelActionsVar (IntMap.delete name)
+    atomicModifyIORef'_ cancelActionsVar $ IntMap.insert name (cancel actAsync)
+    return actAsync
