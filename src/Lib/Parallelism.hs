ad90ff4167cd1e90337b562ea7887a338e6f26ca
Eyal Lotem <eyal.lotem@gmail.com>
2014-04-16 03:49:58 +0300

    Convert Parallelism cell to be MVar: Fix over-parallelism bug

--- src/Lib/Parallelism.hs
+++ src/Lib/Parallelism.hs
@@ -6,13 +6,13 @@ module Lib.Parallelism
   , withReleased
   ) where
 
-import Data.IORef
+import Control.Concurrent.MVar
 import Lib.PoolAlloc (PoolAlloc)
 import qualified Control.Exception as E
 import qualified Lib.PoolAlloc as PoolAlloc
 
 type ParId = Int
-type Cell = IORef ParId
+type Cell = MVar ParId
 type Parallelism = PoolAlloc ParId
 
 new :: ParId -> IO Parallelism
@@ -21,12 +21,12 @@ new n = PoolAlloc.new [1..n]
 startAlloc :: Parallelism -> IO ((Cell -> IO r) -> IO r)
 startAlloc parallelism = do
   alloc <- PoolAlloc.startAlloc parallelism
-  return $ E.bracket (newIORef =<< alloc) (release parallelism)
+  return $ E.bracket (newMVar =<< alloc) (release parallelism)
 
 release :: Parallelism -> Cell -> IO ()
 release parallelism cell = do
   -- Make sure parId is valid with forced evaluation
-  parId <- E.evaluate =<< atomicModifyIORef cell ((,) (error "Attempt to read released resource"))
+  parId <- takeMVar cell
   E.mask_ $ PoolAlloc.release parallelism parId
 
 -- | Release the currently held item, run given action, then regain
@@ -35,4 +35,4 @@ withReleased :: Cell -> Parallelism -> IO a -> IO a
 withReleased cell parallelism =
   E.bracket_
   (release parallelism cell)
-  (writeIORef cell =<< PoolAlloc.alloc parallelism)
+  (putMVar cell =<< PoolAlloc.alloc parallelism)
