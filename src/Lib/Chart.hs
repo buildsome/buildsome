2610f00c6a3914cc9e254aadafb420f96ede8540
Eyal Lotem <eyal.lotem@elastifile.com>
2014-04-29 14:36:28 +0300

    Add Charts flag to .cabal

--- src/Lib/Chart.hs
+++ src/Lib/Chart.hs
@@ -1,17 +1,22 @@
+{-# LANGUAGE CPP #-}
 module Lib.Chart
   ( make
   ) where
 
+import Lib.FilePath (FilePath)
+import Prelude hiding (FilePath)
+import qualified Lib.Slave as Slave
+
+#ifdef WITH_CHARTS_SUPPORT
+
 import Control.Monad (void)
 import Data.Default.Class (def)
 import Lib.FilePath (FilePath)
-import Prelude hiding (FilePath)
 import qualified Data.ByteString.Char8 as BS8
 import qualified Data.Map as M
 import qualified Graphics.Rendering.Chart as Chart
 import qualified Graphics.Rendering.Chart.Backend.Cairo as ChartCairo
 import qualified Lib.BuildMaps as BuildMaps
-import qualified Lib.Slave as Slave
 
 buildTimes :: Slave.Stats -> Chart.PieChart
 buildTimes (Slave.Stats stats) =
@@ -32,3 +37,10 @@ make stats filePath =
       , ChartCairo._fo_size = (2048, 2048)
       }
     plot = def { Chart._pie_plot = buildTimes stats }
+
+#else
+
+make :: Slave.Stats -> FilePath -> IO ()
+make _ _ = fail "Re-build buildsome with the Charts flag enabled to use charts support"
+
+#endif
