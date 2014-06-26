4a4a3880497dd86dac9030320f72c9ec9cfc1fc0
Eyal Lotem <eyal.lotem@elastifile.com>
2014-06-26 14:30:40 +0300

    Add Lib.Map: filterA, filterAWithKey

---
+++ src/Lib/Map.hs
@@ -0,0 +1,19 @@
+module Lib.Map
+  ( filterA, filterAWithKey
+  ) where
+
+import Control.Applicative ((<$>), Applicative)
+import Data.Map (Map)
+import qualified Data.Map as Map
+
+filterA :: Applicative f => (a -> f Bool) -> Map k a -> f (Map k a)
+filterA = filterAWithKey . const
+
+filterAWithKey :: Applicative f => (k -> a -> f Bool) -> Map k a -> f (Map k a)
+filterAWithKey predicate =
+  fmap (Map.mapMaybe removeFalse) .
+  Map.traverseWithKey annotatePredicate
+  where
+    annotatePredicate k x = (,) x <$> predicate k x
+    removeFalse (_, False) = Nothing
+    removeFalse (x, True) = Just x
