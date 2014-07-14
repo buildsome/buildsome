5bb4b8b49f22acc2745ab095519027bbdcffcee1
Eyal Lotem <eyal.lotem@elastifile.com>
2014-07-15 00:27:57 +0300

    Avoid going through Rational in NominalDiffTime/DiffTime instances

--- src/Lib/TimeInstances.hs
+++ src/Lib/TimeInstances.hs
@@ -1,14 +1,32 @@
+{-# LANGUAGE DeriveGeneric, StandaloneDeriving #-}
 {-# OPTIONS -fno-warn-orphans #-}
 module Lib.TimeInstances () where
 
 import Control.Applicative ((<$>))
 import Data.Binary(Binary(..))
 import Data.Time.Clock (NominalDiffTime, DiffTime)
+import Data.Fixed (Pico, Fixed(..))
+import GHC.Generics (Generic)
+
+deriving instance Generic (Fixed a)
+instance Binary (Fixed a)
+
+{-# INLINE toPico #-}
+toPico :: Real a => a -> Pico
+toPico = realToFrac
+
+{-# INLINE fromPico #-}
+fromPico :: Fractional a => Pico -> a
+fromPico = realToFrac
 
 instance Binary NominalDiffTime where
-  put = put . toRational
-  get = fromRational <$> get
+  put = put . toPico
+  get = fromPico <$> get
+  {-# INLINE get #-}
+  {-# INLINE put #-}
 
 instance Binary DiffTime where
-  put = put . toRational
-  get = fromRational <$> get
+  put = put . toPico
+  get = fromPico <$> get
+  {-# INLINE get #-}
+  {-# INLINE put #-}
