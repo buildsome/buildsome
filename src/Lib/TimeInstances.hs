a81e5fd24e77a5ff2e7add33ebfa126b12c89149
Eyal Lotem <eyal.lotem@gmail.com>
2014-07-15 03:57:20 +0300

    Restore support for GHC-7.6.3

--- src/Lib/TimeInstances.hs
+++ src/Lib/TimeInstances.hs
@@ -1,15 +1,29 @@
-{-# LANGUAGE DeriveGeneric, StandaloneDeriving #-}
+{-# LANGUAGE CPP, DeriveGeneric, StandaloneDeriving #-}
 {-# OPTIONS -fno-warn-orphans #-}
 module Lib.TimeInstances () where
 
 import Control.Applicative ((<$>))
 import Data.Binary(Binary(..))
 import Data.Time.Clock (NominalDiffTime, DiffTime)
-import Data.Fixed (Pico, Fixed(..))
-import GHC.Generics (Generic)
+import Data.Fixed (Pico, Fixed(..), HasResolution)
 
+#if __GLASGOW_HASKELL__ <= 706
+fromFixed :: HasResolution a => Fixed a -> Integer
+fromFixed = truncate . (*1e12)
+{-# INLINE fromFixed #-}
+toFixed :: HasResolution a => Integer -> Fixed a
+toFixed = (/1e12) . fromIntegral
+{-# INLINE toFixed #-}
+instance HasResolution a => Binary (Fixed a) where
+  get = toFixed <$> get
+  put = put . fromFixed
+  {-# INLINE get #-}
+  {-# INLINE put #-}
+#else
+import GHC.Generics (Generic)
 deriving instance Generic (Fixed a)
 instance Binary (Fixed a)
+#endif
 
 {-# INLINE toPico #-}
 toPico :: Real a => a -> Pico
