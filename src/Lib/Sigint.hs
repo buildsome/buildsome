c78f55ee7aace220624b8d45ee805e09b9cbaf9a
Eyal Lotem <eyal.lotem@elastifile.com>
2015-02-26 11:49:14 +0200

    Ctrl-C runs handler just once

--- src/Lib/Sigint.hs
+++ src/Lib/Sigint.hs
@@ -2,19 +2,22 @@ module Lib.Sigint (withInstalledSigintHandler) where
 
 import Control.Concurrent.Async (withAsync)
 import Control.Concurrent.MVar
+import Control.Monad (void)
 import Lib.Exception (bracket)
+import Lib.Once (once)
 import System.Posix.ByteString (Handler(..), keyboardSignal, installHandler)
 
 withInstalledSigintHandler :: IO () -> IO a -> IO a
 withInstalledSigintHandler action body =
     do
+        runOnce <- once
+        let install mvar = handleSigint $ Catch $ void $ runOnce $ putMVar mvar ()
         mvar <- newEmptyMVar
         withAsync (takeMVar mvar >> action) $ \_ ->
             bracket (install mvar) uninstall $ const body
     where
         handleSigint handler =
             installHandler keyboardSignal handler Nothing
-        install mvar = handleSigint $ Catch $ putMVar mvar ()
         uninstall oldHandler =
             do
                 _ <- handleSigint oldHandler
