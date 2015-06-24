cc734dd6cbaebc5cc8f5f96c2f401ca51fdddde4
Eyal Lotem <eyal.lotem@elastifile.com>
2015-06-25 01:06:47 +0300

    Setup.hs: -Wall cleanup

--- Setup.hs
+++ Setup.hs
@@ -2,10 +2,11 @@ import Distribution.Simple
 import Distribution.Simple.Setup
 import Distribution.Simple.Program
 import Distribution.Simple.Program.Db
-import Distribution.Verbosity
 
+sources :: [FilePath]
 sources = ["cbits/fs_override.c", "cbits/canonize_path.c", "cbits/client.c"]
 
+main :: IO ()
 main = defaultMainWithHooks simpleUserHooks
   { preBuild = \_ flags -> do
       let verbosity = fromFlag $ buildVerbosity flags
