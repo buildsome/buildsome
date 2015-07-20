45988b078dd7b0d45a452eda83d93cc68a43ede5
Eyal Lotem <eyal.lotem@elastifile.com>
2015-07-20 22:30:57 +0300

    TimeIt: Add printTimeIt, pureTimeIt

--- src/Lib/TimeIt.hs
+++ src/Lib/TimeIt.hs
@@ -1,12 +1,27 @@
 module Lib.TimeIt
-  ( timeIt
-  ) where
+    ( timeIt
+    , printTimeIt
+    , pureTimeIt
+    ) where
 
+import Control.Exception (evaluate)
 import Data.Time (getCurrentTime, diffUTCTime, NominalDiffTime)
+import System.IO.Unsafe
 
 timeIt :: IO a -> IO (NominalDiffTime, a)
-timeIt act = do
-  before <- getCurrentTime
-  res <- act
-  after <- getCurrentTime
-  return (after `diffUTCTime` before, res)
+timeIt act =
+    do
+        before <- getCurrentTime
+        res <- act
+        after <- getCurrentTime
+        return (after `diffUTCTime` before, res)
+
+printTimeIt :: String -> IO a -> IO a
+printTimeIt msg act =
+    do
+        (t, res) <- timeIt act
+        putStrLn $ msg ++ " took " ++ show t
+        return res
+
+pureTimeIt :: String -> a -> a
+pureTimeIt msg x = unsafePerformIO $ printTimeIt msg (evaluate x)
