0b80ab0423555d411c55473fe9d186e5014bdd1c
Noam Lewis <noam.lewis@elastifile.com>
2015-08-04 15:27:15 +0300

    Revert "New Lib.TimeInstances"
    This reverts commit c0eaa7d615035243877c237ce3c315b05a51bf39.

--- src/Lib/TimeInstances.hs
+++ src/Lib/TimeInstances.hs
@@ -3,24 +3,46 @@
 {-# OPTIONS -fno-warn-orphans #-}
 module Lib.TimeInstances () where
 
-import Data.Binary (Binary(..))
-import Data.Fixed (Fixed(..))
-import Data.Time.Clock (NominalDiffTime, DiffTime)
-import GHC.Generics (Generic)
-
 import Prelude.Compat
 
-deriving instance Generic (Fixed a)
-instance Binary (Fixed a)
+import Data.Binary(Binary(..))
+import Data.Time.Clock (NominalDiffTime, DiffTime)
+import Data.Fixed (Pico, Fixed(..), E12)
+
+toPicos :: Pico -> Integer
+fromPicos :: Integer -> Pico
+#if __GLASGOW_HASKELL__ <= 706
+toPicos = truncate . (*1e12)
+fromPicos = (/1e12) . realToFrac
+#else
+toPicos (MkFixed x) = x
+fromPicos = MkFixed
+#endif
+{-# INLINE toPicos #-}
+{-# INLINE fromPicos #-}
+
+instance Binary (Fixed E12) where
+  get = fromPicos <$> get
+  put = put . toPicos
+  {-# INLINE get #-}
+  {-# INLINE put #-}
+
+{-# INLINE toPico #-}
+toPico :: Real a => a -> Pico
+toPico = realToFrac
+
+{-# INLINE fromPico #-}
+fromPico :: Fractional a => Pico -> a
+fromPico = realToFrac
 
 instance Binary NominalDiffTime where
-  put = put . toRational
-  get = fromRational <$> get
+  put = put . toPico
+  get = fromPico <$> get
   {-# INLINE get #-}
   {-# INLINE put #-}
 
 instance Binary DiffTime where
-  put = put . toRational
-  get = fromRational <$> get
+  put = put . toPico
+  get = fromPico <$> get
   {-# INLINE get #-}
   {-# INLINE put #-}
