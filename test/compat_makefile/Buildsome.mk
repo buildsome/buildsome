4d49dd09e5400d89ba43687d28229edadbfef0b3
Noam Lewis <jones.noamle@gmail.com>
2015-09-03 19:01:14 +0300

    compat makefile: escape filenames

--- test/compat_makefile/Buildsome.mk
+++ test/compat_makefile/Buildsome.mk
@@ -5,7 +5,13 @@ COMPILE=gcc -c -o $@ $<
 %.o: %.c
 	${COMPILE}
 
-default: foo.o moshe.a moshe.a.b
+default: foo.o moshe.a moshe.a.b escape_input
 
 %.a %.a.b:
 	cat foo.c > $@ ; cat foo.c >"$@.b"
+
+escape(bla):
+	echo hi > escape\(bla\)
+
+escape_input:
+	cat escape\(bla\) > escape_input
