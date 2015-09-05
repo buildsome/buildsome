2d07cf35ca80545afcb59504ac389a21ae120e84
Noam Lewis <jones.noamle@gmail.com>
2015-09-06 00:37:38 +0300

    compat makefile: improve test

--- test/compat_makefile/Buildsome.mk
+++ test/compat_makefile/Buildsome.mk
@@ -5,7 +5,7 @@ COMPILE=gcc -c -o $@ $<
 %.o: %.c
 	${COMPILE}
 
-default: foo.o moshe.a moshe.a.b escape_input
+default: foo.o moshe.a moshe.a.b escape_input should_be_deleted_by_make
 
 %.a %.a.b:
 	cat foo.c > $@ ; cat foo.c >"$@.b"
@@ -15,3 +15,6 @@ escape(bla):
 
 escape_input:
 	cat escape\(bla\) > escape_input
+
+should_be_deleted_by_make: should_be_deleted_by_make_dependency
+	echo "# bla" >> should_be_deleted_by_make
