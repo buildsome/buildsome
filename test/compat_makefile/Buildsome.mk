be58b5f4f88d535ac154cb8716c2396e3b6e0068
Noam Lewis <jones.noamle@gmail.com>
2015-09-06 09:05:02 +0300

    compat makefile: use "$@" in test

--- test/compat_makefile/Buildsome.mk
+++ test/compat_makefile/Buildsome.mk
@@ -1,6 +1,6 @@
 .PHONY: default
 
-COMPILE=gcc -c -o $@ $<
+COMPILE=gcc -c -o "$@" $<
 
 %.o: %.c
 	${COMPILE}
@@ -8,13 +8,16 @@ COMPILE=gcc -c -o $@ $<
 default: foo.o moshe.a moshe.a.b escape_input should_be_deleted_by_make
 
 %.a %.a.b:
-	cat foo.c > $@ ; cat foo.c >"$@.b"
+	cat foo.c > "$@" ; cat foo.c >"$@.b"
 
 escape(bla=12):
-	echo hi > escape\(bla\=12\)
+	echo hi > "$@"
+
+escape(bla=123,foo='123'):
+	echo hi > "$@"
 
 escape_input:
-	cat escape\(bla=12\) > escape_input
+	cat escape\(bla=12\) escape\(bla=123,foo=\'123\'\) > "$@"
 
 should_be_deleted_by_make: should_be_deleted_by_make_dependency
-	echo "# bla" >> should_be_deleted_by_make
+	echo "# bla" >> "$@"
