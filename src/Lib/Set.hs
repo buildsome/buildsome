c3500dedbb3b9465341cef84d0d4565d1c10d3db
Noam Lewis <jones.noamle@gmail.com>
2015-09-06 09:05:30 +0300

    Lib: add List.partitionA, refactor Set.partitionA
    * Eyal: Changed List.partitionA to be simple&monomorphicm Set.partitionA
      to just convert to/from list

--- src/Lib/Set.hs
+++ src/Lib/Set.hs
@@ -3,21 +3,17 @@ module Lib.Set
   ( filterA, partitionA
   ) where
 
-
-
-import Prelude.Compat
-
-import Data.Set (Set)
-
+import           Data.Set (Set)
 import qualified Data.Set as S
 import qualified Lib.List as L
+
+import           Prelude.Compat
+
+both :: (a -> b) -> (a, a) -> (b, b)
+both f (x, y) = (f x, f y)
 
 filterA :: (Applicative f, Ord k) => (k -> f Bool) -> Set k -> f (Set k)
 filterA p = fmap S.fromAscList . L.filterA p . S.toAscList
 
 partitionA :: (Applicative f, Ord k) => (k -> f Bool) -> Set k -> f (Set k, Set k)
-partitionA p = fmap mconcat . traverse onEach . S.toAscList
-  where
-    onEach x = partitionOne x <$> p x
-    partitionOne x True  = (S.singleton x, S.empty)
-    partitionOne x False = (S.empty, S.singleton x)
+partitionA p = fmap (both S.fromAscList) . L.partitionA p . S.toAscList
