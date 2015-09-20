7c7989d0109fe1034c79656cbd643296020d8850
Noam Lewis <jones.noamle@gmail.com>
2015-09-20 18:00:04 +0300

    improve error message in assertion

--- src/Buildsome/CompatMakefile.hs
+++ src/Buildsome/CompatMakefile.hs
@@ -115,8 +115,11 @@ onOneTarget phoniesSet cwd stats target =
     targetRep = BuildMaps.computeTargetRep target
     directDeps = Stats.tsDirectDeps targetStats
     targetStats =
-      fromMaybe (error "BUG: Stats does not contain targets that appear as root/dependencies") $
-      Map.lookup targetRep (Stats.ofTarget stats)
+      fromMaybe
+      (error $ mconcat
+       [ "BUG: Stats does not contain targets that appear as root/dependencies: "
+       , show $ BuildMaps.targetRepPath targetRep ])
+      $ Map.lookup targetRep (Stats.ofTarget stats)
     depBuildCommands = onMultipleTargets phoniesSet cwd stats directDeps
 
 onMultipleTargets :: Phonies -> FilePath -> Stats -> [Target] -> M [ByteString]
