a65fb8012b2f0c82d3cc39cc0291f891271fd558
Noam Lewis <jones.noamle@gmail.com>
2015-11-16 11:32:39 +0200

    tests/compat_makefile: change to show a bug in creation of directories (tests fail now)

--- test/compat_makefile/Buildsome.mk
+++ test/compat_makefile/Buildsome.mk
@@ -2,10 +2,13 @@
 
 COMPILE=gcc -c -o "$@" $<
 
-%.o: %.c
+out:
+	mkdir out
+
+out/%.o: %.c
 	${COMPILE}
 
-default: foo.o moshe.a moshe.a.b escape_input should_be_deleted_by_make
+default: out/foo.o moshe.a moshe.a.b escape_input should_be_deleted_by_make
 
 %.a %.a.b:
 	cat foo.c > "$@" ; cat foo.c >"$@.b"
