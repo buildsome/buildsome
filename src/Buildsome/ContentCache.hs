7b8f6a1b88bfc525939de2c6672685b3a047d4e2
Noam Lewis <noam.lewis@elastifile.com>
2016-01-28 01:38:49 +0200

    cache: better messages, place files in subdirs aa, ab, etc.

--- src/Buildsome/ContentCache.hs
+++ src/Buildsome/ContentCache.hs
@@ -10,10 +10,11 @@ import           Buildsome.Types (Buildsome(..), BuildTargetEnv(..))
 import           Control.Monad (unless, when, forM_)
 import           Control.Monad.IO.Class (MonadIO(..))
 import           Control.Monad.Trans.Either (EitherT(..), left)
+import qualified Data.ByteString.Lazy as BS
+import qualified Data.ByteString.Char8 as BS8
 import qualified Data.ByteString.Base16 as Base16
 import           Data.List (sortOn)
 import           Data.Maybe (fromMaybe)
-
 import           Data.Monoid
 import           Data.String (IsString(..))
 import           Lib.Directory (getMFileStatus)
@@ -55,19 +56,22 @@ filesToDelete maxSize fs = foldr go (0, []) fs
                     then (fileName, fileSize):outFiles
                     else outFiles
 
+toKb :: Integral a => a -> a
+toKb x = x `div` 1024
+
 cleanContentCacheDir :: Buildsome -> IO ()
 cleanContentCacheDir buildsome = do
   Dir.createDirectories $ contentCacheDir buildsome
   putStr "Checking cache dir size..."
   savedSize <- Db.readIRef $ Db.cachedOutputsUsage (bsDb buildsome)
   case savedSize of
-      Just x | x < const_MAX_CACHE_SIZE -> putStrLn "OK."
+      Just x | x < const_MAX_CACHE_SIZE -> putStrLn $ "OK, " <> show (toKb $ const_MAX_CACHE_SIZE - x) <> "kb left before cleanup."
       _ -> cleanContentCacheDir' buildsome
 
 cleanContentCacheDir' :: Buildsome -> IO ()
 cleanContentCacheDir' buildsome = do
   files <- Dir.getDirectoryContents (contentCacheDir buildsome)
-      >>= mapM (return . ((contentCacheDir buildsome <> "/") <>))
+      >>= mapM (return . (contentCacheDir buildsome </>))
       >>= mapM (\fileName -> (fileName,) <$> getMFileStatus fileName)
   let (totalSize, filesToRemove) = filesToDelete const_MAX_CACHE_SIZE $ sortOn (fmap Posix.modificationTimeHiRes . snd) files
       numRemoved = length $ filesToRemove
@@ -76,27 +80,35 @@ cleanContentCacheDir' buildsome = do
       then putStrLn $ concat
            [ "Cache dir ", show (contentCacheDir buildsome)
            , " contains ", show (length files)
-           , " files, totaling ", show totalSize
-           , " bytes. Going to remove ", show numRemoved, " oldest cache files"
-           , ", saving ", show bytesSaved, " bytes." ]
+           , " files, totaling ", show (toKb totalSize)
+           , "kb. Going to remove ", show numRemoved, " oldest cache files"
+           , ", saving ", show (toKb $ bytesSaved), "kb." ]
       else putStr "Updating from disk..." >> putStrLn "OK."
   forM_ filesToRemove (Posix.removeLink . fst)
   Db.writeIRef (Db.cachedOutputsUsage (bsDb buildsome)) $ totalSize - bytesSaved
 
 mkTargetWithHashPath :: Buildsome -> Hash -> FilePath
-mkTargetWithHashPath buildsome contentHash = contentCacheDir buildsome </> Base16.encode (Hash.asByteString contentHash)-- (outPath <> "." <> Base16.encode contentHash)
+mkTargetWithHashPath buildsome contentHash = contentCacheDir buildsome </> prefix </> name
+  where (prefix, name) = BS8.splitAt 2 $ Base16.encode (Hash.asByteString contentHash)
+
+placeInCache :: FilePath -> FilePath -> IO ()
+placeInCache inFile outFile = Posix.createLink inFile outFile
+
+getFromCache :: FilePath -> FilePath -> IO ()
+getFromCache inFile outFile = Posix.createLink inFile outFile
 
 addFileToCache :: Buildsome -> FilePath -> Posix.FileStatus -> Hash -> IO ()
-addFileToCache buildsome outPath stat contentHash = do
+addFileToCache buildsome outPath _stat contentHash = do
   let targetPath = mkTargetWithHashPath buildsome contentHash
   -- putStrLn $ BS8.unpack ("Caching: " <> outPath <> " -> " <> targetPath)
   alreadyExists <- FilePath.exists targetPath
   unless alreadyExists $ do
     Dir.createDirectories $ FilePath.takeDirectory targetPath
-    Dir.copyFile outPath targetPath
+    placeInCache outPath targetPath
+    cachedFileStat <- Posix.getFileStatus targetPath
     savedSize <- fromMaybe 0 <$> Db.readIRef (Db.cachedOutputsUsage $ bsDb buildsome)
     Db.writeIRef (Db.cachedOutputsUsage (bsDb buildsome))
-        $ savedSize + (fromIntegral $ Posix.fileSize stat)
+        $ savedSize + (fromIntegral $ Posix.fileSize cachedFileStat)
 
 refreshFromContentCache :: (IsString e, MonadIO m) =>
   BuildTargetEnv -> FilePath -> Maybe FileContentDesc -> Maybe FileStatDesc -> EitherT e m ()
@@ -106,7 +118,7 @@ refreshFromContentCache
     unless oldExists $ left "No cached copy"
   liftIO $ do
     printStrLn btePrinter $ bsRender bteBuildsome
-      $ mconcat [ "Copying: " <> cPath (show cachedPath) <> " -> " <> cPath (show filePath) ]
+      $ mconcat [ "Restoring: " <> cPath (show cachedPath) <> " -> " <> cPath (show filePath) ]
     removeIfExists filePath
     removeIfExists tempFile
     -- Update the cached file's mtime so as to make cache cleanup by last usage possible
@@ -116,7 +128,7 @@ refreshFromContentCache
     -- Set stat attributes before creating the file, so the target file is created with correct
     -- attrs from the start
     -- TODO use proper tempfile naming
-    Dir.copyFile cachedPath  tempFile
+    getFromCache cachedPath tempFile
     -- TODO set other stat fields?
     -- TODO these may cause failure later (over permissions) if the user running now is not the one
     -- recorded in the log!
