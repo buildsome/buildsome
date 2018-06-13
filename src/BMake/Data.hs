ff5476e0d2a3d03a0fde1f713393bc0617ceb993
Eyal Lotem <eyal.lotem@gmail.com>
2018-06-13 18:29:27 +0300

    MetaVar stringify

--- src/BMake/Data.hs
+++ src/BMake/Data.hs
@@ -1,6 +1,7 @@
 module BMake.Data
-    ( MetaVar(..)
-    , MetaVarModifier(..)
+    ( MetaVar(..), metaVarChar
+    , MetaVarModifier(..), metaVarModifierChar
+    , metaVarString
     , Expr3(..) -- ToDo: rename
     ) where
 
@@ -16,32 +17,53 @@ import           GHC.Generics
 --------------------------------------------------------------------------------
 
 data MetaVar
-  = FirstOutput
-  | FirstInput
-  | AllInputs
-  | AllOOInputs
-  | Stem
-  deriving (Eq, Ord, Show, Generic)
+    = FirstOutput
+    | FirstInput
+    | AllInputs
+    | AllOOInputs
+    | Stem
+    deriving (Eq, Ord, Show, Generic)
 instance ToJSON MetaVar where -- ToDo: remove when not needed
 instance Binary MetaVar where -- ToDo: remove when not needed
 instance NFData MetaVar where
-  rnf = genericRnf
+    rnf = genericRnf
+
+metaVarChar :: MetaVar -> Char
+metaVarChar FirstOutput = '@'
+metaVarChar FirstInput = '<'
+metaVarChar AllInputs = '^'
+metaVarChar AllOOInputs = '|'
+metaVarChar Stem = '*'
 
 data MetaVarModifier
-  = NoMod
-  | ModFile
-  | ModDir
-  deriving (Eq, Ord, Show, Generic)
+    = NoMod
+    | ModFile
+    | ModDir
+    deriving (Eq, Ord, Show, Generic)
 instance ToJSON MetaVarModifier where -- ToDo: remove when not needed
 instance Binary MetaVarModifier where -- ToDo: remove when not needed
 instance NFData MetaVarModifier where
-  rnf = genericRnf
+    rnf = genericRnf
+
+metaVarModifierChar :: MetaVarModifier -> Maybe Char
+metaVarModifierChar NoMod = Nothing
+metaVarModifierChar ModFile = Just 'F'
+metaVarModifierChar ModDir = Just 'D'
+
+metaVarString :: MetaVar -> MetaVarModifier -> String
+metaVarString metaVar modifier =
+    '$' :
+    case metaVarModifierChar modifier of
+    Nothing -> [varChar]
+    Just modChar -> ['(',varChar,modChar,')']
+    where
+        varChar = metaVarChar metaVar
 
 data Expr3 -- ToDo: rename
-  = Expr3'Str ByteString
-  | Expr3'Spaces
-  | Expr3'VarSpecial MetaVar MetaVarModifier
-  deriving (Eq, Ord, Show, Generic)
+    = Expr3'Str ByteString
+    | Expr3'Spaces
+    | Expr3'VarSpecial MetaVar MetaVarModifier
+    deriving (Eq, Ord, Show, Generic)
 instance NFData Expr3
 instance Binary Expr3 where -- ToDo: remove when not needed
 
