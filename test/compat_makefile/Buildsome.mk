bfc97756a6e646017c13a2599ad593c913e0bcce
Eyal Lotem <eyal.lotem@gmail.com>
2019-04-01 14:16:04 +0300

    Bring tests to pass again

--- test/compat_makefile/Buildsome.mk
+++ test/compat_makefile/Buildsome.mk
@@ -23,4 +23,4 @@ escape_input:
 	cat escape\(bla=12\) escape\(bla=123,foo=\'123\'\) > "$@"
 
 should_be_deleted_by_make: should_be_deleted_by_make_dependency
-	echo "# bla" >> "$@"
+	echo "true" >> "$@"
